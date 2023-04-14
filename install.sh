b62f6dfe4dd6216806cc8c80cc0fbe03b40f9d4e
Mister R <misterr-labs@tutanota.com>
2023-04-14 15:25:48 +0200

    Fixed bug. Forgot exit 0

--- install.sh
+++ install.sh
@@ -100,6 +100,7 @@ copy_installer_or_continue_session() {
   if [[ -d "${installer_home}" ]]; then
     if [[ -f "${installer_home}/.installsessionstate" ]] && [[ "$(cat "${installer_home}/.installsessionstate")" = "${installer_state[finished_eqsnode_install]}" ]]; then
       echo -e "\033[0;33mA finished installation of an Equilibria service node has been found! This installation script is ONLY for fresh installations not for updating a service node.\033[0m"
+      exit 0
     fi
 
     while true; do
