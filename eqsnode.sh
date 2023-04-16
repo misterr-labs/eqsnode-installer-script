ec10ed85871d7c55ae3930169528e914ad5aaf0d
Mister R <misterr-labs@tutanota.com>
2023-04-16 12:03:03 +0200

    Removed some commented out code

--- eqsnode.sh
+++ eqsnode.sh
@@ -23,6 +23,7 @@ port_params=
 if [[ "${config[multi_node]}" -eq 1 ]]; then
   port_params="--zmq-rpc-bind-port ${config[zmq_rpc_bind_port]} --p2p-bind-port ${config[p2p_bind_port]} --rpc-bind-port ${config[rpc_bind_port]}"
 fi
+
 active_user=${USER:=$(/usr/bin/id -run)}
 readonly active_user
 
@@ -132,11 +133,7 @@ build_and_install_service_file() {
   fi
 
   echo -e "\n\033[1mGenerating service file '${service_file}'...\033[0m"
-#  local port_params=''
-#
-#  if [[ "${config[multi_node]}" -eq 1 ]]; then
-#    port_params=" --zmq-rpc-bind-port ${config[zmq_rpc_bind_port]} --p2p-bind-port ${config[p2p_bind_port]} --rpc-bind-port ${config[rpc_bind_port]}"
-#  fi
+
   # shellcheck disable=SC2002
   cat "${service_template}" | sed -e "s/%INSTALL_USERNAME%/${config[running_user]}/g" -e "s#%INSTALL_ROOT%#${install_root_bin_dir}#g" -e "s/%PORT_PARAMS%/${port_params}/g" | sudo tee "${service_file}"
 
@@ -197,8 +194,7 @@ watch_daemon_status() {
 
   echo -e "\n\033[1mMonitoring blockchain download progress by daemon:\033[0m"
   setterm -cursor off
-#  local port_params
-#  port_params="--zmq-rpc-bind-port ${config[zmq_rpc_bind_port]} --p2p-bind-port ${config[p2p_bind_port]} --rpc-bind-port ${config[rpc_bind_port]}"
+
   while true; do
     read blocks_done total_blocks perc <<< "$(~/bin/daemon status ${port_params} | grep -o 'Height:.*' | sed -n 's/^Height: \([0-9]*\)\/\([0-9]*\) (\([0-9.]*\).*/\1 \2 \3/p')"
 
